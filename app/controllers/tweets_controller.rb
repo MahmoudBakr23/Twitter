dcf8d9a43dc378025acbb1b44c4b0747296c4081
Mahmoud Bakr <mbakr6821@gmail.com>
2020-09-24 02:46:57 +0200

    last modifications

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order("created_at DESC")
     @tweet = Tweet.new
   end
 
